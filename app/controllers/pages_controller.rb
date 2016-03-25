abab419ebba4a9b8105876b85b3b4939741bc781
Yusuke Oshiro <yusuke@oshiro1.com>
2016-03-25 09:32:16 +0900

    added ajax interface for loading activity

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,6 +24,9 @@ class PagesController < ApplicationController
 	end
 
 	def prospect
+		
+
+
 		uri = URI.parse('https://pi.pardot.com/api/login/version/3') 
 		email    = URI.encode_www_form_component( ENV["PARDOT_EMAIL"] )
 		password = URI.encode_www_form_component( ENV["PARDOT_PASSWORD"] )
@@ -38,7 +41,8 @@ class PagesController < ApplicationController
 		response = JSON.parse(res.body)
 
 		api_key = response["api_key"]
-		p api_key
+		session["api_key"] = api_key
+		p "apikey = #{api_key}"
 		prospect_email = URI.encode_www_form_component( params[:email] )
 
 
@@ -53,14 +57,69 @@ class PagesController < ApplicationController
 		response = JSON.parse(res.body)
 	    
 		@prospect = response["prospect"]
-
 		@prospect["visitor_activities"]["visitor_activity"].sort! do |a, b|  b["created_at"] <=> a["created_at"] end
-		#@prospect[]listEvents.sort! do |a, b|  a["created_at"] <=> b["created_at"] end
-	    #render :json => response
+	
+	end
+
+	def visits
+		activity_id = params[:activity_id]
+		uri = URI.parse("https://pi.pardot.com/api/VisitorActivity/version/3/do/read/id/#{activity_id}") 
+		key      = URI.encode_www_form_component( ENV["PARDOT_KEY"] )
+		api_key  = URI.encode_www_form_component( session["api_key"] )
+		param = "api_key=#{api_key}&user_key=#{key}&format=json"		
+		https = Net::HTTP.new(uri.host, 443)
+		https.use_ssl = true
+		https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+		res = https.post(
+		    uri.request_uri, param
+		)
+		response = JSON.parse(res.body)
+
+		visitor_id = response["visitor_activity"]["visitor_id"]
+		uri = URI.parse("https://pi.pardot.com/api/visitor/version/3/do/read/id/#{visitor_id}") 
+		https = Net::HTTP.new(uri.host, 443)
+		https.use_ssl = true
+		https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+		res = https.post(
+		    uri.request_uri, param
+		)
+		response = JSON.parse(res.body)
 
 
+		@page_views = Array.new
+		response["visitor"]["visitor_activities"]["visitor_activity"].each do |a|
+			if a["type"] == 20
+				p "...iterating...."
+				visit_id = a["visit_id"]
+				uri = URI.parse("https://pi.pardot.com/api/visit/version/3/do/read/id/#{visit_id}") 
+				https = Net::HTTP.new(uri.host, 443)
+				https.use_ssl = true
+				https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+				res = https.post(
+				    uri.request_uri, param
+				)
+				response = JSON.parse(res.body)
+
+				p "::::::::::::::::::"
+				element = nil
+				if response["visit"]["visitor_page_views"]["visitor_page_view"].instance_of?(Array)
+					element = response["visit"]["visitor_page_views"]["visitor_page_view"]
+				else
+					element = [response["visit"]["visitor_page_views"]["visitor_page_view"]]
+				end
+				
+				@page_views = @page_views + element
+				p element
+				p @page_views.length
 
 
+			end
+
+		end
+		
+		@page_views.sort! do |a, b|  b["created_at"] <=> a["created_at"] end		
+
+		render :json => @page_views
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   #get 'login', :to => "pages#login"
   get 'prospect', :to => "pages#prospect"
-
+  get 'activity/:activity_id', :to => "pages#visits"
 
 
 end
